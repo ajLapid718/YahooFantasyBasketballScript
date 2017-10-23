f328ec2637aa3fb0fb2cebd4061635f4e5b6d1c1
Allan James Lapid <ajLapid718@gmail.com>
2017-10-23 03:47:52 -0400

    Match for exact string content and start active players

--- Start_Active_Players.rb
+++ Start_Active_Players.rb
@@ -1,3 +1,4 @@
+# import the watir gem/library...formerly known as 'watir-webdriver'
 require 'watir'
 
 # Opens the Google Chrome Web Browser
@@ -10,19 +11,19 @@ browser.goto("https://basketball.fantasysports.yahoo.com")
 browser.element(:id => 'yucs-profile').click
 
 # Types in Email Address
-browser.text_field(:id => 'login-username').set("TYPE_IN_YAHOO_EMAIL_HERE")
+browser.text_field(:id => 'login-username').set("ENTER_EMAIL")
 
 # Clicks on Next
 browser.element(:id => 'login-signin').click
 
 # Types in Password
-browser.text_field(:id => 'login-passwd').set("TYPE_IN_PASSWORD_HERE")
+browser.text_field(:id => 'login-passwd').set("ENTER_PASSWORD")
 
 # Clicks on Sign In
 browser.element(:id => 'login-signin').click
 
 # Click on My Team
-browser.div(text: /My Team/).click
+browser.element(:class => 'F-link', text: /^YOUR_TEAM_NAME$/).click
 
 # Click on Start Active Players
-browser.div(text: /Start Active Players/).click
+browser.element(:class => ["Btn", "Btn-short", "Btn-primary", "Mend-med"], text: /^Start Active Players$/).click
