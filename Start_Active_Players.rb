a1e82655006c058008014cdff2cc7f8e8ab7cb6e
Allan James Lapid <ajLapid718@gmail.com>
2017-10-23 02:36:36 -0400

    Start up the browser and reach MyTeam

--- Start_Active_Players.rb
+++ Start_Active_Players.rb
@@ -1,1 +1,26 @@
-Placeholder
+# Opens the Google Chrome Web Browser
+browser = Watir::Browser.new :chrome
+
+# Goes to Web Address
+browser.goto("https://basketball.fantasysports.yahoo.com")
+
+# Clicks on Sign In
+browser.element(:id => 'yucs-profile').click
+
+# Types in Email Address
+browser.text_field(:id => 'login-username').set("TYPE_IN_YAHOO_EMAIL_HERE")
+
+# Clicks on Next
+browser.element(:id => 'login-signin').click
+
+# Types in Password
+browser.text_field(:id => 'login-passwd').set("TYPE_IN_PASSWORD_HERE")
+
+# Clicks on Sign In
+browser.element(:id => 'login-signin').click
+
+# Click on My Team
+browser.div(text: /My Team/).click
+
+# Click on Start Active Players
+browser.div(text: /Start Active Players/).click
