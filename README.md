0291f30180575a4ff81da3c852c040b87a908f95
Allan James Lapid <ajLapid718@gmail.com>
2017-10-23 04:43:32 -0400

    Throw some refreshes in there and click on next day

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 When I figure out what's going on, you'll know what's going on.
+
+I have some basic functionality going on.
+There's a lot of improvements that can be done.
+But I'm pretty happy with what I've done so far.
+
+I'll try to work on this for the week.
+Looking to get a range/loop going to handle the repetitive logic at the bottom.
+Going to need to get user input.
+Going to need to make sure the password gets typed as a hidden text in the terminal.
+
+Going to see how far I can take this in a week.
+This was the MVP so anything from here is a major plus to me.
--- Start_Active_Players.rb
+++ Start_Active_Players.rb
@@ -11,19 +11,31 @@ browser.goto("https://basketball.fantasysports.yahoo.com")
 browser.element(:id => 'yucs-profile').click
 
 # Types in Email Address
-browser.text_field(:id => 'login-username').set("ENTER_EMAIL")
+browser.text_field(:id => 'login-username').set("YOUR_EMAIL_ADDRESS")
 
 # Clicks on Next
 browser.element(:id => 'login-signin').click
 
 # Types in Password
-browser.text_field(:id => 'login-passwd').set("ENTER_PASSWORD")
+browser.text_field(:id => 'login-passwd').set("YOUR_PASSWORD")
 
 # Clicks on Sign In
 browser.element(:id => 'login-signin').click
 
 # Click on My Team
 browser.element(:class => 'F-link', text: /^YOUR_TEAM_NAME$/).click
+
+# Refresh the Browser
+browser.refresh
+
+# Click on Start Active Players
+browser.element(:class => ["Btn", "Btn-short", "Btn-primary", "Mend-med"], text: /^Start Active Players$/).click
+
+# Click on the Small Next Day Forward Arrow Icon
+browser.element(:class => "Js-next").click
+
+# Refresh the Browser
+browser.refresh
 
 # Click on Start Active Players
 browser.element(:class => ["Btn", "Btn-short", "Btn-primary", "Mend-med"], text: /^Start Active Players$/).click
