4bae123e790dc651ad5df0bc8a57a8fec65dd165
Allan James Lapid <ajLapid718@gmail.com>
2017-10-23 20:38:35 -0400

    Describe the range of dates covered

--- Start_Active_Players.rb
+++ Start_Active_Players.rb
@@ -1,5 +1,6 @@
 # Import the watir gem/library...formerly known as 'watir-webdriver'
 require 'watir'
+require 'date'
 
 # Opens the Google Chrome Web Browser
 browser = Watir::Browser.new(:chrome)
@@ -30,14 +31,16 @@ browser.element(:class => 'F-link', text: /^YOUR_TEAM_NAME$/).click
 
 # Set Active Players for X Amount of Days (Default is 7)
 number_of_days = 7
+puts "Currently setting active players from #{Time.now.strftime("%B, %d, %Y")} to #{(DateTime.now + number_of_days).strftime("%B, %d, %Y")}"
 number_of_days.times do
 	browser.element(:class => ["Btn", "Btn-short", "Btn-primary", "Mend-med"], text: /^Start Active Players$/).click
 	browser.element(:class => "Js-next").click
 end
 
 # Farewell Messages and Closing the Browser Session
-puts "Everything is all set!"
-sleep(2)
+sleep(1)
+puts "Everything is all set for #{number_of_days} days!"
+sleep(1)
 puts "Goodbye!"
 sleep(1)
 browser.close
