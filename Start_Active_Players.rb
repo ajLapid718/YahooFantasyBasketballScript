563ab4d2331e72792df35d06040ff0727c45d1ab
Allan James Lapid <ajLapid718@gmail.com>
2017-10-26 01:57:23 -0400

    Test out spacing issue attempt

--- Start_Active_Players.rb
+++ Start_Active_Players.rb
@@ -27,19 +27,19 @@ TEAM_NAME_OF_YOUR_OPPONENT = gets.chomp
 browser = Watir::Browser.new(:chrome, switches: %w[--log-level=3 --headless])
 
 begin
-	browser.goto("https://basketball.fantasysports.yahoo.com")
+browser.goto("https://basketball.fantasysports.yahoo.com")
 rescue
-	puts "There's been an error regarding accessing the website. This is a troubling start."
+puts "There's been an error regarding accessing the website. This is a troubling start."
 end
 
 begin
-	browser.element(:id => 'yucs-profile').click
+browser.element(:id => 'yucs-profile').click
 rescue
-	puts "There's been an error regarding clicking on the Sign In link. Will refresh and try again!"
-	sleep(1)
-	browser.refresh
-	sleep(1)
-	retry
+puts "There's been an error regarding clicking on the Sign In link. Will refresh and try again!"
+sleep(1)
+browser.refresh
+sleep(1)
+retry
 end
 
 puts " "
