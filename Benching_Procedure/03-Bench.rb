acec6710be5166d979dc79696148cab003a580ba
Allan James Lapid <ajLapid718@gmail.com>
2017-10-27 06:08:23 -0400

    Use spaces instead of tabs

--- Benching_Procedure/03-Bench.rb
+++ Benching_Procedure/03-Bench.rb
@@ -1,27 +1,27 @@
 def click_on_my_team
-	begin
-		@browser.element(:class => 'F-link', text: MY_TEAM).click
-	rescue
-		puts "There's been an error regarding clicking on your team name."
-		sleep(1)
-		@browser.refresh
-		sleep(1)
-		retry
-	end
+  begin
+    @browser.element(:class => 'F-link', text: MY_TEAM).click
+  rescue
+    puts "There's been an error regarding clicking on your team name."
+    sleep(1)
+    @browser.refresh
+    sleep(1)
+    retry
+  end
 end
 
 def bench_roster
-	puts " "
-	puts "Currently benching all players for #{Time.now.strftime("%B, %d, %Y")}"
-	10.times do |i|
-		player_name = @browser.table(:id => "statTable0")[i+2][2]
-		next if player_name.text == "(Empty)"
-		puts "Currently putting #{player_name.text} on the bench!"
-		sleep(3)
-		player_name.click
-		sleep(3)
-		@browser.elements(:css, "tr[data-pos='BN']").last.click
-	end
+  puts " "
+  puts "Currently benching all players for #{Time.now.strftime("%B, %d, %Y")}"
+  10.times do |i|
+    player_name = @browser.table(:id => "statTable0")[i+2][2]
+    next if player_name.text == "(Empty)"
+    puts "Currently putting #{player_name.text} on the bench!"
+    sleep(3)
+    player_name.click
+    sleep(3)
+    @browser.elements(:css, "tr[data-pos='BN']").last.click
+  end
 end
 
 # Invoke Bench All Players Functionality
