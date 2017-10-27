9a674dacd89a9debacbd82939d34ee60cdb0cd18
Allan James Lapid <ajLapid718@gmail.com>
2017-10-27 06:03:55 -0400

    Use spaces instead of tabs for cleanup

--- Benching_Procedure/01-Info.rb
+++ Benching_Procedure/01-Info.rb
@@ -4,23 +4,23 @@ def get_email
 end
 
 def get_password
-	begin
-		yahoo_password = ask("Enter your password: ") { |q| q.echo = "*" }
-		password_confirmation_attempt = ask("Confirm your password: ") { |q| q.echo = "*" }
-		if yahoo_password == password_confirmation_attempt
-			return yahoo_password
-		else
-			raise Exception.new("Your passwords do not match. Try again.")
-		end
-	rescue Exception => error_message
-		puts "#{error_message}"
-		retry
-	end
+  begin
+    yahoo_password = ask("Enter your password: ") { |q| q.echo = "*" }
+    password_confirmation_attempt = ask("Confirm your password: ") { |q| q.echo = "*" }
+    if yahoo_password == password_confirmation_attempt
+      return yahoo_password
+    else
+      raise Exception.new("Your passwords do not match. Try again.")
+    end
+  rescue Exception => error_message
+    puts "#{error_message}"
+    retry
+  end
 end
 
 def get_team_name
-	print "Enter the name of your team: "
-	return gets.chomp
+  print "Enter the name of your team: "
+  return gets.chomp
 end
 
 # Invoke the Acquiring Information Process
