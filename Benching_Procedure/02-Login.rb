c50fc2726b4e640e632d82b4e3b222c7568709fb
Allan James Lapid <ajLapid718@gmail.com>
2017-10-27 06:10:10 -0400

    use two spaces instead of tabbing

--- Benching_Procedure/02-Login.rb
+++ Benching_Procedure/02-Login.rb
@@ -1,33 +1,33 @@
 def start_browser_session
-	@browser = Watir::Browser.new(:chrome, switches: %w[--log-level=3 --headless])
+  @browser = Watir::Browser.new(:chrome, switches: %w[--log-level=3 --headless])
 end
 
 def go_to_yahoo
-	begin
-		@browser.goto("https://basketball.fantasysports.yahoo.com")
-	rescue
-		puts "There's been an error regarding accessing the website. This is a troubling start."
-	end
+  begin
+    @browser.goto("https://basketball.fantasysports.yahoo.com")
+  rescue
+    puts "There's been an error regarding accessing the website. This is a troubling start."
+  end
 end
 
 def click_on_profile
-	begin
-		@browser.element(:id => 'yucs-profile').click
-	rescue
-		puts "There's been an error regarding clicking on the Sign In link. Will refresh and try again!"
-		sleep(1)
-		@browser.refresh
-		sleep(1)
-		retry
-	end
+  begin
+    @browser.element(:id => 'yucs-profile').click
+  rescue
+    puts "There's been an error regarding clicking on the Sign In link. Will refresh and try again!"
+    sleep(1)
+    @browser.refresh
+    sleep(1)
+    retry
+  end
 end
 
 def log_user
-	puts " "
-	puts "Currently logging in..."
-	@browser.tap { |b| b.text_field(:id => 'login-username').set(YAHOO_EMAIL_ADDRESS) }.send_keys(:enter)
-	@browser.tap { |b| b.text_field(:id => 'login-passwd').set(YAHOO_PASSWORD) }.send_keys(:enter)
-	puts "Successfully logged in. Navigating to My Team now."
+  puts " "
+  puts "Currently logging in..."
+  @browser.tap { |b| b.text_field(:id => 'login-username').set(YAHOO_EMAIL_ADDRESS) }.send_keys(:enter)
+  @browser.tap { |b| b.text_field(:id => 'login-passwd').set(YAHOO_PASSWORD) }.send_keys(:enter)
+  puts "Successfully logged in. Navigating to My Team now."
 end
 
 # Invoke Login Process
