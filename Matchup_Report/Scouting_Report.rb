ee5b0fe7045a555c2d0612d433e60ae5690cdc0b
Allan James Lapid <ajLapid718@gmail.com>
2017-11-11 17:55:40 -0500

    Map out the possible methods to be used

--- Matchup_Report/Scouting_Report.rb
+++ Matchup_Report/Scouting_Report.rb
@@ -1,2 +1,38 @@
 require 'watir'
 require 'highline/import'
+
+def get_email
+end
+
+def get_password
+end
+
+def start_browser_session
+end
+
+def click_on_sign_in
+end
+
+def enter_email
+end
+
+def enter_password
+end
+
+def click_on_league
+end
+
+def generate_report
+end
+
+def shutdown
+end
+
+def grab_all_stats
+end
+
+if __FILE__ == $PROGRAM_NAME
+  EMAIL = get_email
+  PASSWORD = get_password
+  grab_all_stats
+end
